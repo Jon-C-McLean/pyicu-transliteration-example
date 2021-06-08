0e4bac0c856c813485ead619f3a08c4c583016cd
Jon McLean <jon@mclean.one>
2021-06-08 10:28:21 +1000

    Added polyglot and test input files

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-import polyglot as pg
+from polyglot.text import Text
 
+script_file = open("input/Thai-Script.txt", "r")
+
+script_text = script_file.read()
+polytext = Text(script_text)
+
+for x in polytext.transliterate("en"):
+    print(x)
