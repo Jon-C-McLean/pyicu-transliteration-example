7073c08e5ae3f435d3d6d5ebf12ddbd21411a84d
Jon McLean <jon@mclean.one>
2021-06-08 11:16:30 +1000

    Added transcription output parsing

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from polyglot.text import Text
+import json as j
 
 script_file = open("input/Thai-Script.txt", "r")
 
 script_text = script_file.read()
 polytext = Text(script_text)
 
-transliterated_script = ""
+transcription_output = open("input/output.json", "r").read()
+json = j.loads(transcription_output)
+video_words = json['results'][0]['alternatives'][0]['words']
+
+script_output = []
+video_output = []
 
 for x in polytext.transliterate("en"):
-    transliterated_script += str(x)
+    script_output += [str(x)]
 
-    if str(x) != " ":
-        transliterated_script += " "
+for x in video_words:
+    t = Text(x['word'])
+    word = t.transliterate("en")[0]
 
-print(transliterated_script)
+    if word != None:
+        video_output += [(word, x['startTime'], x['endTime'])]
+
+
+script_output = list(filter(None, script_output))
+
